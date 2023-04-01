bf84c9fbcc0fb76c7efc18b4d092a84082490b7c
Alice <lamalice9496@gmail.com>
2023-03-31 22:50:28 -0600

    Completed version of ages in minutes

---
+++ app/src/main/java/com/example/agetominutescalculator/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.agetominutescalculator
+
+import android.app.DatePickerDialog
+import android.icu.text.SimpleDateFormat
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.example.agetominutescalculator.databinding.ActivityMainBinding
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.DatePickerButton.setOnClickListener {clickDatePicker() }
+    }
+
+    private fun clickDatePicker(){
+
+        val myCalendar = Calendar.getInstance()
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+        val year = myCalendar.get(Calendar.YEAR)
+        val dpd = DatePickerDialog(
+            this,
+            DatePickerDialog.OnDateSetListener{ _, year, month, dayOfMonth ->
+                Toast.makeText(this, "Date picker button $month",Toast.LENGTH_LONG).show()
+                binding.SelectedDate.text = "${month+1}/$dayOfMonth/$year"
+
+                val selectedDate = "${month+1}/$dayOfMonth/$year"
+
+                val sdf = SimpleDateFormat("dd/mm/yyy", Locale.ENGLISH)
+                val theDate = sdf.parse(selectedDate)
+                val selectedDateInMinutes = theDate.time / 60000
+                val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+                val currentDateInMinutes = currentDate.time / 60000
+                val timeDifferent = currentDateInMinutes - selectedDateInMinutes
+
+                binding.ResultValue?.text = "${binding.InputNameField.text} is ${timeDifferent.toString()} minutes old"
+            },
+            year, month, day
+        )
+        dpd.datePicker.maxDate = System.currentTimeMillis() - 86400000
+        dpd.show()
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
